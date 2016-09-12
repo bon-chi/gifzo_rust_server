3f23ebec5626a5e010ba3540e116a034b96f3d1d
Koji Ota <afterjnih@gmail.com>
2016-09-12 21:08:41 +0900

    set routing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World")))
-    }).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+
+    router.get("/gifs/:id", get_gif, "id");
+    fn get_gif(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Sorry, there is no gif.")))
+    }
+
+    Iron::new(router).http("localhost:3000").unwrap();
 }

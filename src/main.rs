2b33e7d8abe84f76707b41470b323297149bc4eb
Koji Ota <afterjnih@gmail.com>
2016-09-14 14:36:24 +0900

    multi part

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,73 @@
-extern crate rustc_serialize;
-
 extern crate iron;
-extern crate router;
 extern crate staticfile;
 extern crate mount;
-extern crate handlebars_iron as hbs;
+extern crate multipart;
 
-use rustc_serialize::json::{Json, ToJson};
-use std::collections::BTreeMap;
 use std::path::Path;
+use std::fs::File;
+use std::io::Read;
 
 use iron::prelude::*;
 use iron::status;
-use router::Router;
 use staticfile::Static;
 use mount::Mount;
-use hbs::{Template, HandlebarsEngine, DirectorySource};
 
-struct Giff {
-    title: String,
-    url: String,
-}
-
-impl ToJson for Giff {
-    fn to_json(&self) -> Json {
-        let mut m: BTreeMap<String, Json> = BTreeMap::new();
-        m.insert("title".to_string(), self.title.to_json());
-        m.insert("url".to_string(), self.url.to_json());
-        m.to_json()
-    }
-}
-
+use multipart::server::{Multipart, Entries, SaveResult};
 
 fn main() {
-    let mut hbse = HandlebarsEngine::new();
-    hbse.add(Box::new(DirectorySource::new("src/templates/", ".hbs")));
-    if let Err(r) = hbse.reload() {
-        panic!("hoge");
-    }
-
-    let mut router = Router::new();
-    router.get("/gifs/:id", get_gif, "id");
-    fn get_gif(_: &mut Request) -> IronResult<Response> {
-        let mut resp = Response::new();
-        let data = Giff {
-            title: "gif11".to_string(),
-            url: "gif/giphy.gif".to_string(), 
-        };
-        resp.set_mut(Template::new("index", data)).set_mut(status::Ok);
-        println!{"{:?}",resp};
-        Ok(resp)
-    }
-
-    let mut chain = Chain::new(router);
-    chain.link_after(hbse);
-
     let mut mounts = Mount::new();
-    mounts.mount("/", chain).mount("gifs/gif/", Static::new(Path::new("src/templates/gif/")));
-
-    // Iron::new(router).http("localhost:3000").unwrap();
-    // Iron::new(chain).http("localhost:3000").unwrap();
+    mounts.mount("/gifs/", Static::new(Path::new("src/templates/gif/")))
+        .mount("/", process_request);
     Iron::new(mounts).http("localhost:3000").unwrap();
+}
+
+fn process_request(request: &mut iron::prelude::Request) -> IronResult<Response> {
+    // Getting a multipart reader wrapper
+    match Multipart::from_request(request) {
+        Ok(mut multipart) => {
+            // Fetching all data and processing it.
+            // save_all() reads the request fully, parsing all fields and saving all files
+            // in a new temporary directory under the OS temporary directory.
+            match multipart.save_all() {
+                SaveResult::Full(entries) => process_entries(entries),
+                SaveResult::Partial(entries, error) => {
+                    try!(process_entries(entries));
+                    Err(IronError::new(error, status::InternalServerError))
+                }
+                SaveResult::Error(error) => Err(IronError::new(error, status::InternalServerError)),
+            }
+        }
+        Err(_) => Ok(Response::with((status::BadRequest, "The request is not multipart"))),
+    }
+}
+
+/// Processes saved entries from multipart request.
+/// Returns an OK response or an error.
+fn process_entries(entries: Entries) -> IronResult<Response> {
+    for (name, field) in entries.fields {
+        println!(r#"Field "{}": "{}""#, name, field);
+    }
+
+    for (name, savedfile) in entries.files {
+        let filename = match savedfile.filename {
+            Some(s) => s,
+            None => "None".into(),
+        };
+        let mut file = match File::open(savedfile.path) {
+            Ok(file) => file,
+            Err(error) => {
+                return Err(IronError::new(error,
+                                          (status::InternalServerError,
+                                           "Server couldn't save file")))
+            }
+        };
+        let mut contents = String::new();
+        if let Err(error) = file.read_to_string(&mut contents) {
+            return Err(IronError::new(error, (status::BadRequest, "The file was not a text")));
+        }
+
+        println!(r#"Field "{}" is file "{}":"#, name, filename);
+        println!("{}", contents);
+    }
+    Ok(Response::with((status::Ok, "Multipart data is processed")))
 }
